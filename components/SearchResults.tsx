da61d3127eae7ccb3699b1354713160965ef04ca
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-08-01 22:48:30 -0300

    UseMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 type SearchResultsProps ={
@@ -9,8 +10,10 @@ type SearchResultsProps ={
 }
 
 export function SearchResults({results }: SearchResultsProps) {
+    const totalPrice = useMemo(() => results.reduce((total, product) => total + product.price, 0),[results]);
     return (
         <div>
+            <h2>{totalPrice}</h2>
             {results.map((product) => (
                 <ProductItem key={product.id} product={product} />
                 ))}
